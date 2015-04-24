e61550c9e4208bd17ee00f6c76dda3a60db2811a
Joannazyy <joannazyy@yahoo.com>
2015-04-23 23:04:03 -0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,29 @@
+<?php
+include 'head.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+h1{
+	margin-top: 50px;
+	text-align: center;
+
+}
+
+body {
+    background-image: url("library cloud.jpg");
+    background-size: 800px 400px;
+    background-repeat: no-repeat;
+    background-attachment: scroll ;
+    background-position: 50% -80%; 
+}
+</style>
+</head>
+<body>
+
+<h1>Welcome To Library Management System Platform</h1>
+
+</body>
+</html>
