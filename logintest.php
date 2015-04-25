4a578e4d596855da874204f3941a1ce2159c55b7
Joannazyy <joannazyy@yahoo.com>
2015-04-25 00:23:36 -0700

    Update logintest.php

--- logintest.php
+++ logintest.php
@@ -34,7 +34,7 @@ include 'head.php';
 
           $check_query = mysql_query($check);
 
-          $check_result = mysql_fetch_array($check_query);
+          #$check_result = mysql_fetch_array($check_query);
 
           echo "<table class='table table-striped table-bordered' style='margin-top: 10px'><tr>
                <th>ID</th>
@@ -42,13 +42,49 @@ include 'head.php';
                <th>Issue Date</th>
                <th>Due Date</th>
                </tr>";
-          if ($row = $check_result) {
+          while ($row = mysql_fetch_array($check_query)) {
                echo "<tr>";
                echo "<td>" . $row['ID'] . "</td><td>"
                    ."<a href='items.php?number=".$row['ID']."'>". $row['Book_title'] . "</a></td><td>"
                    . $row['Issue_date'] . "</td><td>"
                    . $row['Due_date'] . "</td>";
                echo "</tr>";
+          }
+          echo "</table>";
+
+          echo "<br /><h3>Your events schedule!</h3>";
+
+          $find="SELECT * From membership_activity WHERE ( id='".$id."')";
+
+          $find_query = mysql_query($find);
+
+          #$find_result = mysql_fetch_assoc($find_query);
+
+          echo "<table class='table table-striped table-bordered' style='margin-top: 10px'><tr>
+               <th>ID</th>
+               <th>Number</th>
+               <th>Date</th>
+               <th>Start time</th>
+               <th>End time</th>
+               <th>Event name</th>
+               <th>Age group</th>
+               <th>Event type</th>
+               <th>Event description</th>
+             </tr>";
+          if (mysql_num_rows($find_query)>0) {
+               while ($row=mysql_fetch_assoc($find_query)){
+               echo "<tr>";
+               echo "<td>" . $row['id'] . "</td><td>"
+                   ."<a href='items.php?number=".$row['id']."'>". $row['Number'] . "</a></td><td>"
+                   . $row['Date_of_event'] . "</td><td>"
+                   . $row['Start_time_of_event'] . "</td><td>"
+                   . $row['End_time_of_event'] . "</td><td>"
+                   . $row['Event_name'] . "</td><td>"
+                   . $row['Age_group'] . "</td><td>"
+                   . $row['Event_type'] . "</td><td>"
+                   . $row['Event_description'] . "</td>";
+               echo "</tr>";
+               }
           }
           echo "</table>";
 
@@ -63,5 +99,5 @@ include 'head.php';
 
      }
 
-
+mysql_close($conn);
 ?>
