8f0c37734d6937aabcf95eb4c36c5816bdc24aa1
Joannazyy <joannazyy@yahoo.com>
2015-04-24 17:36:33 -0700

    Create loginform.php

---
+++ loginform.php
@@ -0,0 +1,20 @@
+<?php
+include 'head.php';
+?>
+
+<h3>Membership Login</h3>
+
+<html>
+      <form action="logintest.php" method="post">
+      	<label for="name">ID</label><br />
+      	<input type="text" name="id"><br />
+      	<label for="name">Username</label><br />
+      	<input type="text" name="name"><br />
+      	<label for="name">Password</label><br />
+      	<input type="password" name="pwd"><br />
+      	<br /><input type="submit">
+      </form>	    
+
+
+
+</html>
