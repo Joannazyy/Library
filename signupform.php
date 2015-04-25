81c5c6259526b51ad7b2992548fb71c399b57b6d
Joannazyy <joannazyy@yahoo.com>
2015-04-24 17:39:02 -0700

    Create signupform.php

---
+++ signupform.php
@@ -0,0 +1,19 @@
+<?php
+include 'head.php';
+?>
+
+<h3>Membership Sign Up</h3>
+
+
+<html>
+      <form action="signuptest.php" method="post">
+      	<label for="name">Username</label><br />
+      	<input type="text" name="n"><br />
+      	<label for="name">Password</label><br />
+      	<input type="password" name="p"><br />
+      	<label for="name">ID</label><br />
+      	<input type="text" name="id"><br />
+      	<br /><input type="submit">
+      </form>
+
+</html>
